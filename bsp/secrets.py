6d22a86349cc1f840e082a73fe5cfa9852602cfe
Aidan Murray <aidanm0121@gmail.com>
2020-05-13 17:55:32 -0400

    Delete secrets.py

--- bsp/secrets.py
+++
@@ -1,7 +0,0 @@
-
-chromePath = r'C:\webdrivers\chromedriver.exe'
-myUsername = '852135'
-myPassword = 'Harper!2003'
-ePath = r'D:\CodeProjects\VisualStudio repos\BeautifulSoupPractice\bsp\extension.crx'
-#print('username = '+ myUsername + '\n' + 'password = ' + myPassword )
-
