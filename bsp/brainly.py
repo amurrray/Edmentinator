7379b02bacbc536af219bed6d5e26c1ee9bec8dd
Sulaiman Ghori <skghori03@gmail.com>
2020-05-19 20:10:23 -0400

    answers databasing complete as well as query

--- bsp/brainly.py
+++ bsp/brainly.py
@@ -1,15 +1,8 @@
 import logging
-import os
-import requests
-from requests_html import HTMLSession
 import pickle
-from random import randint
-from lxml.html import fromstring
 from pathlib import Path
-from time import sleep
 
-from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
+from fuzzywuzzy import fuzz, process
 
 # setup logging
 logging.basicConfig(level=logging.INFO, format=('%(asctime)s %(levelname)s %(name)s | %(message)s'))
@@ -20,56 +13,51 @@ logger.setLevel(logging.DEBUG)
 BASE_URL = "https://brainly.com/"
 DEBUG = True
 
-def get_proxies():
-    url = 'https://free-proxy-list.net/'
-    response = requests.get(url)
-    parser = fromstring(response.text)
-    proxies = []
-    for i in parser.xpath('//tbody/tr')[:10]:
-        if i.xpath('.//td[7][contains(text(),"yes")]'):
-            #Grabbing IP and corresponding PORT
-            proxy = ":".join([i.xpath('.//td[1]/text()')[0],
-                              i.xpath('.//td[2]/text()')[0]])
-            proxies.append(proxy)
-    return proxies
+# pickle.dump([{'question': 'where was george washington born', 'answer': 'westmoreland country virginia va'}], open('answers.pkl', 'wb'))
 
-proxies = get_proxies()
-PROXY = proxies[randint(0, (len(proxies) - 1))]
-logger.debug(PROXY)
+def query(question, specificness=90):
+    '''
+    returns an object in the format {'question': question, 'answer': answer}
+    if the answer is not found in the database, manual input of the answer
+    is required. click on the url in the terminal and paste the answer
+    into the input
 
-userAgent = UserAgent().random
-logger.debug(userAgent)
+    i know specificness is dumb. thats why i used it.
+    specificness is on a scale from 0 to 100, 0 being everything matches and 100 being only exact matches
+    '''
+    
+    answersDB = pickle.load(open('answers.pkl', 'rb'))
 
-def query(query):
-    # generate query url so that the user can get datadome key
-    query = query.replace(' ', '+')
-    queryUrl = BASE_URL + 'app/ask?entry=top&q=' + query
-    print(queryUrl)
-    answers = pickle.load(open('answers.pkl', 'rb'))
+    # generate list of all known questions
+    logger.debug(answersDB)
+    knownQuestions = []  # perhaps implement stop word filtering later on: https://en.wikipedia.org/wiki/Stop_words
+    for answer in answersDB:
+        knownQuestions.append(answer['question'])
+    logger.debug('all known questions:' + str(knownQuestions))
 
-    # make the request look like it came from a user browser
-    s = requests.session()
-    s = HTMLSession()
-    datadomeKey = input('insert datadome key: ')
-    my_cookie = requests.cookies.create_cookie('datadome', datadomeKey)
-    s.headers.update({'User-Agent': userAgent})
-    s.cookies.set_cookie(my_cookie)
+    # find the closest match to our question
+    foundQuestion = process.extractOne(question, knownQuestions)
+    logger.debug('found question: ' + foundQuestion[0] + ', with confidence ' + str(foundQuestion[1]))
 
-    r = s.get(queryUrl)
-    # THIS DOESNT FKIN WORK CUZ BRAINLY USES JS TO GENERATE THE SEARCH PAGE
+    if foundQuestion[1] < specificness: # if the found question wasnt close enough to our question, get the answer to our question
+        # generate question url so that the user can get datadome key
+        questionUrl = BASE_URL + 'app/ask?entry=top&q=' + question.replace(' ', '+')
 
-    soup = BeautifulSoup(r.text, 'lxml')
+        # make the request look like it came from a user browser
+        print(questionUrl)
+        answerBrainly = input('answer: ')
 
-    questions = soup.find_all('a', href=True)
-    file = open('resp.html', 'w')
-    file.write(r.text)
-    file.close()
-    answers = soup.find_all('a', class_='sg-text sg-text--small sg-text--link sg-text--bold sg-text--blue-dark')
-
-    for answer in answers:
-        print(answer)
-
-    pickle.dump(answers, open('answers.pkl', 'wb'))
+        confirm = input('CONFIRM that the answer TO ' + question + ' IS ' + answerBrainly + '? [y/n] ')
+        if confirm.lower() == 'y':
+            answersDB.append({'question': question, 'answer': answerBrainly})
+            pickle.dump(answersDB, open('answers.pkl', 'wb'))
+            return {'question': question, 'answer': answerBrainly}
+        else:
+            return query(question)
+    else:
+        for answer in answersDB:
+            if foundQuestion[0] == answer['question']:
+                return {'question': answer['question'], 'answer': answer['answer']}
 
 if __name__ == "__main__":
-    query('where was george washington born')
+    print(query('who was thomas jefferson')['answer'])
