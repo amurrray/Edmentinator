6aca7e42f45e91ac7ed6049d1ec13c617e51a10e
Sulaiman Ghori <skghori03@gmail.com>
2020-05-27 11:49:12 -0400

    added flags to database.py and removed scrapy

--- bsp/brainly/brainly/docker.sh
+++
@@ -1,1 +0,0 @@
-docker run -p 5023:5023 -p 8050:8050 -p 8051:8051 scrapinghub/splash
--- bsp/database.py
+++ bsp/database.py
@@ -1,6 +1,7 @@
 import logging
 import pickle
 import json
+import argparse
 
 from pathlib import Path
 from shutil import copyfile
@@ -10,38 +11,57 @@ from printy import printy, inputy
 logging.basicConfig(level=logging.DEBUG, format=('%(asctime)s %(levelname)s %(name)s | %(message)s'))
 logger = logging.getLogger('database')
 
+parser = argparse.ArgumentParser(description='edmentinator database management script')
+parser.add_argument('-i', '--importd', help='imports the database from json file', action='store_true')
+parser.add_argument('-e', '--export', help='exports the database to json file', action='store_true')
+parser.add_argument('-s', '--sync', help='syncs with other members of the edmentinator network', action='store_true')
+
 try:
     pickle.load(open(str(Path(__file__).resolve().parents[0]) + '/answers.pkl', 'rb'))
 except FileNotFoundError:
     pickle.dump([], open('answers.pkl', 'wb'))
 
 def importFromJson():
+    ''' imports db from json file '''
     answers = json.load(open(str(Path(__file__).resolve().parents[0]) + '/answers.json', 'r'))
     copyfile(str(Path(__file__).resolve().parents[0]) + '/answers.pkl', str(Path(__file__).resolve().parents[0]) + '/answers.BACKUP.pkl')
-    print('database backed up to answers.BACKUP.pkl')
+    printy('database backed up to answers.BACKUP.pkl', 'g')
     pickle.dump(answers, open(str(Path(__file__).resolve().parents[0]) + '/answers.pkl', 'wb'))
-    print('answers imported to answers.pkl')
+    printy('answers imported to answers.pkl', 'n')
 
 def exportToJson():
+    ''' exports db to json file '''
     db = pickle.load(open(str(Path(__file__).resolve().parents[0]) + '/answers.pkl', 'rb'))
     json.dump(db, open(str(Path(__file__).resolve().parents[0]) + '/answers.json', 'w'))
-    print('answers exported to answers.json')
+    printy('answers exported to answers.json', 'n')
+
+def syncDB():
+    pass
 
 def main():
-    dbLen = str(len(pickle.load(open(str(Path(__file__).resolve().parents[0]) + '/answers.pkl', 'rb'))))
-    answersLen = str(len(json.load(open(str(Path(__file__).resolve().parents[0]) + '/answers.json', 'r'))))
+    args = parser.parse_args()
 
-    printy('local database has ' + dbLen + ' answers', 'p')
-    printy('json has ' + answersLen + ' answers', 'n')
-    print('')
-
-    sel = inputy('[n]import@ database from json or [p]export@ database to json? \[[n]i@/[p]e@]').lower()
-    if sel == 'i':
-        printy('importing..', 'n')
+    if args.importd:
         importFromJson()
-    elif sel == 'e':
-        printy('exporting..', 'p')
+    elif args.export:
         exportToJson()
+    elif args.sync:
+        syncDB()
+    else:
+        dbLen = str(len(pickle.load(open(str(Path(__file__).resolve().parents[0]) + '/answers.pkl', 'rb'))))
+        answersLen = str(len(json.load(open(str(Path(__file__).resolve().parents[0]) + '/answers.json', 'r'))))
+
+        printy(f'local database has {dbLen} answers', 'p')
+        printy(f'json has {answersLen} answers', 'o')
+        print('')
+
+        sel = inputy('[n]import@ database from json or [p]export@ database to json? \[[n]i@/[p]e@]').lower()
+        if sel == 'i':
+            printy('importing..', 'o')
+            importFromJson()
+        elif sel == 'e':
+            printy('exporting..', 'p')
+            exportToJson()
     
 
 if __name__ == '__main__':
